73676833c08d7c390d766d75e5942dcbe4a6e4f9
Simankov Alexandr <148748481+Fotlex@users.noreply.github.com>
2024-10-22 18:43:26 +1000

    fixed local

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 document.getElementById('lang').onclick = function() {
-    console.log(this.checked);
-
     for (let i = 0; i < pairsArray.length; i++) {
         document.querySelector(pairsArray[i][0]).innerHTML = langArr[pairsArray[i][1]][this.checked ? "ru" : "en"];
     }    
