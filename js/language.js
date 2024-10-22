7045039c6ad33042450e764c344045a85b14a0c7
Simankov Alexandr <148748481+Fotlex@users.noreply.github.com>
2024-10-22 18:39:13 +1000

    language

---
+++ js/language.js
@@ -0,0 +1,67 @@
+const langArr = {
+    "name" : {
+        "ru": "Симанков Александр",
+        "en": "Simankov Alexandr",
+    },
+    "aboutMe": {
+        "ru": "Обо мне",
+        "en": "About me",
+    },
+    "Hobbies": {
+        "ru": "Чем занимаюсь",
+        "en": "Hobbies",
+    },
+    "Contact": {
+        "ru": "Контакты",
+        "en": "Contact me",
+    },
+    "Discription": {
+        "ru": "Рад&nbsp;знакомству,&nbsp;<span>меня&nbsp;Саша&nbsp;зовут!",
+        "en": "Nice&nbsp;to&nbsp;meet&nbsp;you,&nbsp;my&nbsp;name&nbsp;is&nbsp;Sasha!",
+    },
+    "nameFull" : {
+        "ru": "Симанков Александр Вадимович",
+        "en": "Simankov Alexandr Vadimovich",
+    },
+    "first": {
+        "ru": "Студент второго курса Дальневосточного Федерального университета.",
+        "en": "Second year student of the Far Eastern Federal University.",
+    },
+    "second": {
+        "ru": "Стараюсь вести активный образ жизни.",
+        "en": "I try to lead an active lifestyle.",
+    },
+    "third" : {
+        "ru": "Клипаю игрушки на коленке для Яндекс Игр.",
+        "en": "I develop games for Yandex Games.",
+    },
+    "programming" : {
+        "ru": "Программирование",
+        "en": "Programming",
+    },
+    "sport": {
+        "ru": "Спорт",
+        "en": "Sport",
+    },
+    "Other": {
+        "ru": "Другое",
+        "en": "Other",
+    },
+}
+
+const pairsArray = [
+    ['.logo', 'name'],
+    ['.about', 'aboutMe'],
+    ['.hobby', 'Hobbies'],
+    ['.contactMe', 'Contact'],
+    ['.name', 'name'],
+    ['.description', 'Discription'],
+    ['.generalInfo__title', 'nameFull'],
+    ['.first', 'first'],
+    ['.second', 'second'],
+    ['.third', 'third'],
+    ['.second-title', 'Hobbies'],
+    ['.Programming', 'programming'],
+    ['.Sport', 'sport'],
+    ['.Other', 'Other'],
+];
